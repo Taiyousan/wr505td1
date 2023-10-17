3c09623402ac65127b848db2f7015c3daddda5c8
Shams Benhamou <shamsbenhamou@gmail.com>
2023-10-17 16:15:41 +0200

    fix: fix afghanistan flag

--- src/curl/apiFlag.php
+++ src/curl/apiFlag.php
@@ -1,16 +1,32 @@
 <?php
 
-$countries = "https://restcountries.com/v3.1/all";
-
-foreach (json_decode(file_get_contents($countries), true) as $country) {
-    $url_flag = $country["flags"]["png"];
-    $flag = $country["name"]["common"] . ".png";
-    $pathinfo = pathinfo($country['flags']['png']);
-    $img = file_get_contents($country['flags']['png']);
-    $query = curl_init($url_flag);
-    curl_setopt($query, CURLOPT_RETURNTRANSFER, true);
-    $result = curl_exec($query);
-    curl_error($query);
-    curl_close($query);
-    file_put_contents('public/flags/' . $pathinfo['basename'], $img);
+$api = 'https://restcountries.com/v3.1/all';
+//-- curl call
+$ch = curl_init($api);
+//--- on ajoute cette option pour pouvoir traiter / utiliser la réponse de l'API
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//---- ignore SSL certificates
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+//---- on execute le call API
+$response = curl_exec($ch);
+if (curl_errno($ch)) {
+    echo 'ERREUR > ' . curl_error($ch);
+} else {
+    //--- je decode la réponse du point d'API en tableau associatif PHP
+    $data = json_decode($response, true);
+    //--- je parcours le tableau associatif
+    foreach ($data as $item) {
+        //--- URL du drapeau
+        $flagUrl =  $item['flags']['png'];
+        $fileInfo = pathinfo($flagUrl);
+        $extension = $fileInfo['extension'];
+        //---- on prépare un beau nom de fichier
+        $filename = $item['cca2'] . '.' . $extension;
+        //---- on récupére le contenu de l'image
+        $image = file_get_contents($flagUrl);
+        //---- on sauvegarde l'image
+        file_put_contents('../assets/flags/' . $filename, $image);
+        //---- F I N
+    }
 }
